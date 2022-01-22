92d7809fbd11ec73c5b6a6e0a46c8ab45dbadad3
Nguyễn Thành Luân <ntluan19@clc.fitus.edu.vn>
2022-01-22 20:37:10 +0700

    First commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+def main():
+    Khoa = "19127443 - Ho Dang Khoa"
+    Luan = "19127467 - Nguyen Thanh Luan"
+    des = "This is demo code for video"
+    print(Khoa,Luan,des, sep='\n')
+if __name__ == "__main__":
+    main()
+
+
